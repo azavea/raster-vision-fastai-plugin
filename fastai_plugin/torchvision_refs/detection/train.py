d3af5954bbca6f185505a8d0a121d6186b56182d
Lewis Fishgold <lewfish@gmail.com>
2019-07-10 14:03:49 -0400

    update torch vision refs

--- fastai_plugin/torchvision_refs/detection/train.py
+++ fastai_plugin/torchvision_refs/detection/train.py
@@ -11,10 +11,10 @@ import torchvision.models.detection.mask_rcnn
 
 from coco_utils import get_coco, get_coco_kp
 
-from group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_groups
-from engine import train_one_epoch, evaluate
+from .group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_groups
+from .engine import train_one_epoch, evaluate
 
-import utils
+import .utils
 import transforms as T
 
 
